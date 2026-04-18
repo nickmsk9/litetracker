e626110b6065f2c96e6568793feeae6f0424591f
Nick Sevalnev <94682037+nickmsk9@users.noreply.github.com>
2026-04-18 09:17:26 +0300

    * в tags_echo() добавил $tags = ''; * теперь пустые теги не ломают функцию * убрал сбор строки через .=, заменил на массив + implode() * в flash_cloud() добавил $flashtag = '';, чтобы тоже не поймать Undefined variable * в get_tags() сделал безопаснее работу с кэшем и $db->get_row($query)

--- blocks/block-newreleases.php
+++ blocks/block-newreleases.php
@@ -1,108 +1,159 @@
 <?
 /*
 ===================================================================
-LiteTracker Source
-===================================================================
-by jenaDI
 -------------------------------------------------------------------
 Назначение: Новые релизы
 ===================================================================
 */
 
-global $memcache , $db ,  $language ,$config , $PRIV; 
-if (false === ($news_releases = $memcache->get('news_releases'))) {
-	
-	$sql = $db->query("SELECT t.* ,  SUM(tr.seeders) AS seeders  , SUM(tr.leechers) AS leechers 
-				FROM torrents  AS t 
-				LEFT JOIN trackers AS tr ON tr.torrent = t.id 
-				WHERE t.banned = '0' AND t.news='1' AND (infohash <> '' AND image <> '') AND  ADDDATE(t.added, INTERVAL ".$config['releases_news']." DAY) > NOW() GROUP BY t.id   ORDER BY t.added DESC LIMIT 25 ");
-	while($row = $db->get_row($sql) ) 
-		$news_releases[] = $row;
-	$memcache->set('news_releases', $news_releases  , 0, (15*60));		
+global $memcached, $db, $language, $config, $PRIV;
+
+$news_releases = array();
+
+if (isset($memcached) && is_object($memcached)) {
+    $cached_news_releases = $memcached->get('news_releases');
+
+    if ($cached_news_releases !== false && is_array($cached_news_releases)) {
+        $news_releases = $cached_news_releases;
+    }
 }
 
+if (!is_array($news_releases) || empty($news_releases)) {
+    $news_releases = array();
 
-if($news_releases) {
-	
-	echo '<script type="text/javascript" src="public/js/wz_tooltip.js"></script>';  
+    $releases_days = isset($config['releases_news']) ? (int)$config['releases_news'] : 0;
 
-	?>
+    $sql = $db->query("
+        SELECT
+            t.*,
+            SUM(tr.seeders) AS seeders,
+            SUM(tr.leechers) AS leechers
+        FROM torrents AS t
+        LEFT JOIN trackers AS tr ON tr.torrent = t.id
+        WHERE t.banned = '0'
+          AND t.news = '1'
+          AND (t.infohash <> '' AND t.image <> '')
+          AND ADDDATE(t.added, INTERVAL {$releases_days} DAY) > NOW()
+        GROUP BY t.id
+        ORDER BY t.added DESC
+        LIMIT 25
+    ");
 
-	
-	<table align="center">
-	<?
-	$rows = 0;
-	$count = 0;
-	foreach($news_releases AS $row) {
-		$count++;
-		$class = ($rows++ % 2) ? 'row1' : 'row2'; // $row is zero-based 
-		//Номер релиза
-		$id = $row['id'];
-		
-		//Размер файла
-		$size = mksize($row['size']);
-		
-		$descr = array();
-		//Год
-		if($row['year'] != 0) {
-			$descr[] = htmlspecialchars($row['year']);
-		}	
-		//Язык
-		if($row['language'] != '') {
-			$descr[] = htmlspecialchars($row['language']);
-		}
-		//Качество
-		if($row['quality'] != '') {
-			$descr[] = htmlspecialchars($row['quality']);
-		}
-		
-		//Собираем массив
-		$descr = (count($descr) ? implode(' / ' , $descr ) : '');
-		
-		//Обложка
-		$image = $row['image'];
-		
-		//Информация о Категории
-		$category = categories_array($row['id_category']);
+    while ($row = $db->get_row($sql)) {
+        if (!is_array($row)) {
+            continue;
+        }
 
-		//Имя категории
-		$cat_name = htmlspecialchars($category['name']);
+        $news_releases[] = $row;
+    }
 
-		//ID Категории
-		$id_category = $category['id'];
-
-		//Картинка Категории
-		$cat_image = $category['image'];
-
-
-		//Описание
-		$tags  = tags_echo($row['tags']);
-		
-		/////////////////////////////////////////////////////////
-		//Пользователи
-		/////////////////////////////////////////////////////////
-		$user = get_user_info($row['id_user']);
-		//ID пользователя
-		$id_user = $user['id'];
-		//Имя пользователя
-		$user_name = $user['name'];
-		//Класс пользователя
-		$user_class = $user['class'];
-		
-		//Раздают
-		$seeders  = number_format($row['seeders']);
-		
-		//Качают
-		$leechers  = number_format($row['leechers']);
-		
-		require 'templates/'.$config['template'].'/blocks/block.releases.news.php';
-	}
-	
-	echo '</table>';
-	
-	// end_frame();
-	
-	
+    if (isset($memcached) && is_object($memcached)) {
+        $memcached->set('news_releases', $news_releases, 15 * 60);
+    }
 }
 
-?>					
+if (!empty($news_releases) && is_array($news_releases)) {
+
+    echo '<script type="text/javascript" src="public/js/wz_tooltip.js"></script>';
+    ?>
+
+    <table align="center">
+    <?
+    $rows = 0;
+    $count = 0;
+
+    foreach ($news_releases as $row) {
+        if (!is_array($row)) {
+            continue;
+        }
+
+        $count++;
+        $class = ($rows++ % 2) ? 'row1' : 'row2';
+
+        // Номер релиза
+        $id = isset($row['id']) ? (int)$row['id'] : 0;
+
+        // Размер файла
+        $size_value = isset($row['size']) ? (float)$row['size'] : 0;
+        $size = mksize($size_value);
+
+        // Описание релиза
+        $descr = array();
+
+        // Год
+        if (isset($row['year']) && $row['year'] !== '' && (int)$row['year'] !== 0) {
+            $descr[] = htmlspecialchars((string)$row['year'], ENT_QUOTES, 'UTF-8');
+        }
+
+        // Язык
+        if (isset($row['language']) && trim((string)$row['language']) !== '') {
+            $descr[] = htmlspecialchars((string)$row['language'], ENT_QUOTES, 'UTF-8');
+        }
+
+        // Качество
+        if (isset($row['quality']) && trim((string)$row['quality']) !== '') {
+            $descr[] = htmlspecialchars((string)$row['quality'], ENT_QUOTES, 'UTF-8');
+        }
+
+        $descr = !empty($descr) ? implode(' / ', $descr) : '';
+
+        // Обложка
+        $image = isset($row['image']) ? (string)$row['image'] : '';
+
+        // Информация о категории
+        $category = array();
+        if (isset($row['id_category'])) {
+            $category = categories_array((int)$row['id_category']);
+        }
+        if (!is_array($category)) {
+            $category = array();
+        }
+
+        // Имя категории
+        $cat_name = isset($category['name']) ? htmlspecialchars((string)$category['name'], ENT_QUOTES, 'UTF-8') : '';
+
+        // ID категории
+        $id_category = isset($category['id']) ? (int)$category['id'] : 0;
+
+        // Картинка категории
+        $cat_image = isset($category['image']) ? (string)$category['image'] : '';
+
+        // Теги
+        $row_tags = isset($row['tags']) ? (string)$row['tags'] : '';
+        $tags = '';
+        if ($row_tags !== '') {
+            $tags = tags_echo($row_tags);
+        }
+
+        /////////////////////////////////////////////////////////
+        // Пользователь
+        /////////////////////////////////////////////////////////
+        $user = array();
+        if (isset($row['id_user'])) {
+            $user = get_user_info((int)$row['id_user']);
+        }
+        if (!is_array($user)) {
+            $user = array();
+        }
+
+        // ID пользователя
+        $id_user = isset($user['id']) ? (int)$user['id'] : 0;
+
+        // Имя пользователя
+        $user_name = isset($user['name']) ? $user['name'] : '';
+
+        // Класс пользователя
+        $user_class = isset($user['class']) ? $user['class'] : 0;
+
+        // Раздают
+        $seeders = number_format(isset($row['seeders']) ? (int)$row['seeders'] : 0);
+
+        // Качают
+        $leechers = number_format(isset($row['leechers']) ? (int)$row['leechers'] : 0);
+
+        require 'templates/' . $config['template'] . '/blocks/block.releases.news.php';
+    }
+
+    echo '</table>';
+}
+?>
--- system/functions/functions.tags.php
+++ system/functions/functions.tags.php
@@ -1,151 +1,200 @@
 <?php
 /*
 ===================================================================
-LiteTracker Source
-===================================================================
-by jenaDI
 -------------------------------------------------------------------
 Назначение: Функции тегов
 ===================================================================
 */
 
-function get_tags() {
-	global $memcache , $db;
-	$arr = array();
-	
-	if (false === ($res = $memcache->get('tags') ) )
-	{
-		$res = $db->query("SELECT name, howmuch FROM tags WHERE howmuch > 0 ORDER BY RAND() LIMIT 15");
-		$tags_cache = array();
-		while ($cache_data = $db->get_row() )
-			$tags_cache[] = $cache_data;
+function get_tags()
+{
+	global $memcache, $db;
 
-		$memcache->set('tags', $tags_cache  , 0, 24*60*60);
+	$arr = array();
+
+	$res = false;
+	if (isset($memcache) && is_object($memcache)) {
+		$res = $memcache->get('tags');
+	}
+
+	if ($res === false || !is_array($res)) {
+		$query = $db->query("SELECT name, howmuch FROM tags WHERE howmuch > 0 ORDER BY RAND() LIMIT 15");
+		$tags_cache = array();
+
+		while ($cache_data = $db->get_row($query)) {
+			if (is_array($cache_data)) {
+				$tags_cache[] = $cache_data;
+			}
+		}
+
+		if (isset($memcache) && is_object($memcache)) {
+			$memcache->set('tags', $tags_cache, 0, 24 * 60 * 60);
+		}
+
 		$res = $tags_cache;
 	}
 
-    foreach($res as $row) {
-            $arr[trim($row['name'])] = $row['howmuch'];
+	foreach ($res as $row) {
+		if (!is_array($row)) {
+			continue;
+		}
+
+		$name = isset($row['name']) ? trim((string)$row['name']) : '';
+		$howmuch = isset($row['howmuch']) ? (int)$row['howmuch'] : 0;
+
+		if ($name !== '') {
+			$arr[$name] = $howmuch;
+		}
 	}
-	
-    return $arr;
+
+	return $arr;
 }
 
-function cloud($small, $big, $colour = true) {
+function cloud($small, $big, $colour = true)
+{
+	$tags = get_tags();
 
-    $tags = get_tags();
-	
-    if (empty($tags))
-        $data = "Нет тэгов";
-    else {
-        $minimum_count = min(array_values($tags));
-        $maximum_count = max(array_values($tags));
-        $spread = $maximum_count - $minimum_count;
+	if (empty($tags)) {
+		$data = "Нет тэгов";
+	} else {
+		$minimum_count = min(array_values($tags));
+		$maximum_count = max(array_values($tags));
+		$spread = $maximum_count - $minimum_count;
 
-        if($spread == 0) {$spread = 1;}
+		if ($spread == 0) {
+			$spread = 1;
+		}
 
-        $data = '';
+		$data = '';
+		$cloud = array();
 
-        $cloud = array();
-
-        foreach ($tags as $tag => $count) {
-			if(!empty($tag) ) {
+		foreach ($tags as $tag => $count) {
+			if (!empty($tag)) {
 				$size = $small + ($count - $minimum_count) * ($big - $small) / $spread;
-				$colours = array('#003EFF', '#0000FF', '#7EB6FF', '#0099CC', '#62B1F6'); // Диапазон цветов (только для статичного облака)
+				$colours = array('#003EFF', '#0000FF', '#7EB6FF', '#0099CC', '#62B1F6');
 
-               $cloud[] = "<a href=\"browse.php?search=";
-               $cloud[]  =   urlencode($tag) ;
-               $cloud[] =  "&type=tags\" style=\"".($colour ? "color:".$colours[mt_rand(0, 4)]."; " : "")."font-size:". floor($size) . "px;\" rel=\"tag\" title=\"Содержится в ".$count." торрентах\">";
-               $cloud[] =  htmlentities($tag, ENT_QUOTES, 'UTF-8') . "(".$count.")</a>\n";
+				$cloud[] = "<a href=\"browse.php?search=";
+				$cloud[] = urlencode($tag);
+				$cloud[] = "&type=tags\" style=\"" . ($colour ? "color:" . $colours[mt_rand(0, 4)] . "; " : "") . "font-size:" . floor($size) . "px;\" rel=\"tag\" title=\"Содержится в " . (int)$count . " торрентах\">";
+				$cloud[] = htmlentities($tag, ENT_QUOTES, 'UTF-8') . "(" . (int)$count . ")</a>\n";
 			}
+		}
 
+		$data = join($cloud);
+		unset($cloud);
+	}
 
-        }
-        $data = join($cloud);
-        unset($cloud);
-    }
-    return $data;
+	return $data;
 }
 
-function flash_cloud($width, $height, $small, $big) {
-    $divname = 'tagcloud';
-    $soname = 'settings';
-    $movie = './public/swf/tagcloud.swf'; // Путь до флэш-файла
-    $path = './public/js'; // Директория js-скриптов
-    $options['bgcolor'] = 'FFFFFF'; // Цвет фона (HEX)
-    $options['trans'] = 'true'; // Прозрачность (true - включена, false - выключена)
-    $options['tcolor'] = '888888'; // Первый цвет тэгов (HEX)
-    $options['tcolor2'] = '333333'; // Второй цвет тэгов (HEX) 111111
-    $options['hicolor'] = '222222'; // Цвет перемешки (HEX)
-    $options['speed'] = '300'; // Скорость вращения
-    $options['distr'] = 'true';
-    $options['mode'] = 'tags';
+function flash_cloud($width, $height, $small, $big)
+{
+	$divname = 'tagcloud';
+	$soname = 'settings';
+	$movie = './public/swf/tagcloud.swf';
+	$path = './public/js';
 
-    ob_start();
-    echo cloud($small, $big);
-    $tags = urlencode( str_replace( "&nbsp;", " ", ob_get_clean() ) );
+	$options = array();
+	$options['bgcolor'] = 'FFFFFF';
+	$options['trans'] = 'true';
+	$options['tcolor'] = '888888';
+	$options['tcolor2'] = '333333';
+	$options['hicolor'] = '222222';
+	$options['speed'] = '300';
+	$options['distr'] = 'true';
+	$options['mode'] = 'tags';
 
-    $flashtag .= '<script type="text/javascript" src="'.$path.'/swfobject.js"> </script>';
-    $flashtag .= '<div id="'.$divname.'"><p style="display:none;">';
-    $flashtag .= urldecode($tags);
-    $flashtag .= '</p></div>';
-    $flashtag .= '<script type="text/javascript">';
-    $flashtag .= 'var rnumber = Math.floor(Math.random()*9999999);';
-    $flashtag .= 'var '.$soname.' = new SWFObject("'.$movie.'?r="+rnumber, "tagcloudflash", "'.$width.'", "'.$height.'", "9", "#'.$options['bgcolor'].'");';
-    if( $options['trans'] == 'true' ){
-        $flashtag .= $soname.'.addParam("wmode", "transparent");';
-    }
-    $flashtag .= $soname.'.addParam("allowScriptAccess", "always");';
-    $flashtag .= $soname.'.addVariable("tcolor", "0x'.$options['tcolor'].'");';
-    $flashtag .= $soname.'.addVariable("tcolor2", "0x' . ($options['tcolor2'] == "" ? $options['tcolor'] : $options['tcolor2']) . '");';
-    $flashtag .= $soname.'.addVariable("hicolor", "0x' . ($options['hicolor'] == "" ? $options['tcolor'] : $options['hicolor']) . '");';
-    $flashtag .= $soname.'.addVariable("tspeed", "'.$options['speed'].'");';
-    $flashtag .= $soname.'.addVariable("distr", "'.$options['distr'].'");';
-    $flashtag .= $soname.'.addVariable("mode", "'.$options['mode'].'");';
-    $flashtag .= $soname.'.addVariable("tagcloud", "'.urlencode('<tags>') . $tags . urlencode('</tags>').'");';
-    $flashtag .= $soname.'.write("'.$divname.'");';
-    $flashtag .= '</script>';
+	ob_start();
+	echo cloud($small, $big);
+	$tags = urlencode(str_replace("&nbsp;", " ", ob_get_clean()));
 
-    return $flashtag;
+	$flashtag = '';
+	$flashtag .= '<script type="text/javascript" src="' . $path . '/swfobject.js"> </script>';
+	$flashtag .= '<div id="' . $divname . '"><p style="display:none;">';
+	$flashtag .= urldecode($tags);
+	$flashtag .= '</p></div>';
+	$flashtag .= '<script type="text/javascript">';
+	$flashtag .= 'var rnumber = Math.floor(Math.random()*9999999);';
+	$flashtag .= 'var ' . $soname . ' = new SWFObject("' . $movie . '?r="+rnumber, "tagcloudflash", "' . $width . '", "' . $height . '", "9", "#' . $options['bgcolor'] . '");';
+
+	if ($options['trans'] == 'true') {
+		$flashtag .= $soname . '.addParam("wmode", "transparent");';
+	}
+
+	$flashtag .= $soname . '.addParam("allowScriptAccess", "always");';
+	$flashtag .= $soname . '.addVariable("tcolor", "0x' . $options['tcolor'] . '");';
+	$flashtag .= $soname . '.addVariable("tcolor2", "0x' . ($options['tcolor2'] == "" ? $options['tcolor'] : $options['tcolor2']) . '");';
+	$flashtag .= $soname . '.addVariable("hicolor", "0x' . ($options['hicolor'] == "" ? $options['tcolor'] : $options['hicolor']) . '");';
+	$flashtag .= $soname . '.addVariable("tspeed", "' . $options['speed'] . '");';
+	$flashtag .= $soname . '.addVariable("distr", "' . $options['distr'] . '");';
+	$flashtag .= $soname . '.addVariable("mode", "' . $options['mode'] . '");';
+	$flashtag .= $soname . '.addVariable("tagcloud", "' . urlencode('<tags>') . $tags . urlencode('</tags>') . '");';
+	$flashtag .= $soname . '.write("' . $divname . '");';
+	$flashtag .= '</script>';
+
+	return $flashtag;
 }
 
-function simple_cloud($small, $big) {
-    $data = '<style>
-        #tag_cloud a {padding : 3px;text-decoration: none;font-family : verdana;font-weight: normal;}
-        #tag_cloud a:link {text-decoration: none;border : 1px solid transparent;}
-        #tag_cloud a:visited {border : 1px solid transparent;}
-        #tag_cloud a:hover {background: #ddd;border : 1px solid #bbb;}
-        #tag_cloud a:active {background : #fff;border : 1px solid transparent;}
-        #tag_cloud p {line-height : 28px;text-align : justify;}
-		#tag_cloud {width:90%}
+function simple_cloud($small, $big)
+{
+	$data = '<style>
+        #tag_cloud a {padding: 3px; text-decoration: none; font-family: verdana; font-weight: normal;}
+        #tag_cloud a:link {text-decoration: none; border: 1px solid transparent;}
+        #tag_cloud a:visited {border: 1px solid transparent;}
+        #tag_cloud a:hover {background: #ddd; border: 1px solid #bbb;}
+        #tag_cloud a:active {background: #fff; border: 1px solid transparent;}
+        #tag_cloud p {line-height: 28px; text-align: justify;}
+        #tag_cloud {width:90%}
         </style>';
-    $data .= '<div id="tag_cloud">';
-    $data .= '<p>'.cloud($small, $big, true).'</p>';
-    $data .= '</div>';
-    return $data;
+	$data .= '<div id="tag_cloud">';
+	$data .= '<p>' . cloud($small, $big, true) . '</p>';
+	$data .= '</div>';
+
+	return $data;
 }
 
-//Вывод тегов
-function get_tags_type() {
-	
-	if(empty($_COOKIE['tags_module'])) 
-		return simple_cloud(15 , 20);	
-	else 
-		return flash_cloud('100%', '100', '30', '50');	
-} 
+// Вывод тегов
+function get_tags_type()
+{
+	if (empty($_COOKIE['tags_module'])) {
+		return simple_cloud(15, 20);
+	} else {
+		return flash_cloud('100%', '100', '30', '50');
+	}
+}
 
-//Вывод тегов к релизу
+// Вывод тегов к релизу
 function tags_echo($addtags)
 {
-	foreach(explode(",", $addtags) as $tag)
-	{
-		if(!empty($addtags))
-			$tags .= "<a style=\"font-weight:normal;\" href=\"browse.php?search=".urlencode($tag)."&type=tags\">".htmlspecialchars_uni($tag)."</a>, ";
+	$tags = '';
+
+	$addtags = trim((string)$addtags);
+
+	if ($addtags === '') {
+		return 'Нет тэгов';
 	}
-	
-	if ($tags) $tags = substr($tags, 0, -2);
-	if (empty($addtags) ) $tags = "Нет тэгов";
-	
+
+	$tag_list = explode(',', $addtags);
+	$result = array();
+
+	foreach ($tag_list as $tag) {
+		$tag = trim($tag);
+
+		if ($tag === '') {
+			continue;
+		}
+
+		$result[] = "<a style=\"font-weight:normal;\" href=\"browse.php?search=" . urlencode($tag) . "&type=tags\">" . htmlspecialchars_uni($tag) . "</a>";
+	}
+
+	if (!empty($result)) {
+		$tags = implode(', ', $result);
+	}
+
+	if ($tags === '') {
+		$tags = 'Нет тэгов';
+	}
+
 	return $tags;
 }
 
